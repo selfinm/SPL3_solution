1d8a3f53717cbcfd1cd54c76d4e29665dd635d47
Your Name <you@example.com>
2023-02-04 15:54:50 +0200

    initial commit

---
+++ src/main/java/bgu/spl/net/impl/rci/RCIClient.java
@@ -0,0 +1,57 @@
+package bgu.spl.net.impl.rci;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.Socket;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class RCIClient implements Closeable {
+
+    private final ObjectEncoderDecoder encdec;
+    private final Socket sock;
+    private final BufferedInputStream in;
+    private final BufferedOutputStream out;
+    private final Queue<Command<?>> queueSender;
+    private final Queue<Command<?>> queueReceiver;
+
+
+    public RCIClient(String host, int port) throws IOException {
+        sock = new Socket(host, port);
+        encdec = new ObjectEncoderDecoder();
+        in = new BufferedInputStream(sock.getInputStream());
+        out = new BufferedOutputStream(sock.getOutputStream());
+        queueSender = new ConcurrentLinkedQueue<>();
+        queueReceiver = new ConcurrentLinkedQueue<>();
+
+    }
+
+    public void send(Command<?> cmd) throws IOException {
+        out.write(encdec.encode(cmd));
+        out.flush();
+    }
+
+    public Serializable receive() throws IOException {
+        int read;
+        while ((read = in.read()) >= 0) {
+            Serializable msg = encdec.decodeNextByte((byte) read);
+            if (msg != null) {
+                return msg;
+            }
+        }
+
+        throw new IOException("disconnected before complete reading message");
+    }
+
+     
+    @Override
+    public void close() throws IOException {
+        out.close();
+        in.close();
+        sock.close();
+    }
+
+}
---
+++ src/test/java/bgu/spl/net/impl/echo/TestEchoClient.java
@@ -0,0 +1,14 @@
+package bgu.spl.net.impl.echo;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class TestEchoClient {
+
+    @Test
+    public void testFoo() {
+        Assert.assertEquals( 1, EchoClient.foo());
+    }
+
+}
